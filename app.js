620bf4c73bd0a7640759af7009636b8e7fb0abe4
James <jamesrea83@gmail.com>
2017-06-22 16:06:51 +0000

    First commit

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require("express"),
+    path = require("path"),
+    fs = require("fs"),
+    morgan = require("morgan");
+
+
+var app = express(),
+    staticPath = path.join(__dirname, "static");
+
+    
+app.use(morgan("combined"));
+
+
+app.use(express.static(staticPath));
+
+
+app.use(function(req, res, next) {
+    res.status(404);
+    res.send("File not found!");
+})
+
+app.listen(process.env.PORT, function() {
+    console.log("App started on port 'process.env.PORT'");
+});
